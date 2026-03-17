6b5ad4af35a572f368825dcbbcaad7b8bfce2296
thiagodorgo <thiagodorgo@gmail.com>
2026-03-16 22:38:15 -0300

    Fix OCR requests to include session credentials

--- client/src/pages/Scan.tsx
+++ client/src/pages/Scan.tsx
@@ -57,6 +57,7 @@ function inferContentType(file: File) {
 async function requestJson<T>(url: string, options?: RequestInit): Promise<T> {
   const response = await fetch(url, {
     ...options,
+    credentials: "include",
     headers: {
       "Content-Type": "application/json",
       ...options?.headers,
